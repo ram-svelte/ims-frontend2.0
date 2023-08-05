1a2231e3f14943259a1d7084d427c67efa7d8ae7
Yashashvi <yashashvi.pandey@sveltetech.com>
2023-08-05 12:55:47 +0530

    changes

--- src/components/CategoriesCardItems.js
+++ src/components/CategoriesCardItems.js
@@ -2,15 +2,17 @@
 
 import { Link } from 'react-router-dom';
 
-const CardItems = ({title,image}) => {
+const CardItems = ({title,image,id}) => {
 	// console.log(props,'props');
 	return(
+		<Link  to={`/categories/${id}/${title}`}>
 	<div style={{borderRadius:"5px",cursor:"pointer"}} className='card-head'>
 	<div style={{borderRadius:"5px"}}>
 		<img width={200} height={200} src={false?image:"lap.jpg"}/>
 	</div>
 	<div style={{background: "#212121",color:"white",textAlign:"center",padding:"3px"}} className='card-title'>{title}</div>
 	</div>
+	</Link>
 	);
 };
 
